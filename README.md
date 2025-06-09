bb5dd439e6ecf261c3ae2e5f7c5fd1bba1ee3f2a
passthesh3ll <63223659+passthesh3ll@users.noreply.github.com>
2025-06-09 19:05:45 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# AI Subtitle Translator
+
+A multi-thread Python script to translate SubRip (.srt) subtitles using AI (g4f).
+
+## Description
+
+The script takes an input .srt file and translates the subtitle text from a specified input language to a specified output language, using an AI model (GPT-4) from [gpt4free](https://github.com/xtekky/gpt4free). The translation consists of two steps: initial (direct) translation and final adaptation, correcting translation errors and gender mismatches (based on characters names and entire translated text). The final translated subtitles are saved to a new .srt file. No API/token is needed.
+
+## Dependencies
+
+1. Generate a virtual environment
+
+```bash
+python -m venv .venv
+source .venv/bin/activate
+```
+
+2. Install dependences
+
+```bash
+pip install g4f\[all\] argparse pycountry tqdm colorama
+```
+
+## Usage
+
+**Help**:
+
+```bash
+python srt-ai-translator.py -h
+```
+
+**Translation** _(Available languages: [ISO 639-2 Codes](https://www.loc.gov/standards/iso639-2/php/code_list.php))_:
+
+```bash
+python srt-ai-translator.py input.srt eng ita
+```
---
+++ srt-ai-translator.py
@@ -0,0 +1,219 @@
+from g4f.client import Client
+from g4f.Provider import Blackbox
+from tqdm import tqdm
+import os, json, pycountry, argparse, time
+from colorama import init, Fore, Style
+
+def srt_to_dict(file_path):
+    subtitles = []
+    with open(file_path, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+
+    i = 0
+    while i < len(lines):
+        if lines[i].strip().isdigit():
+            subtitle = {}
+            subtitle['id'] = lines[i].strip()
+            i += 1
+            subtitle['time-start'], subtitle['time-end'] = lines[i].strip().split(' --> ')
+            i += 1
+            text_lines = []
+            while i < len(lines) and lines[i].strip():
+                text_lines.append(lines[i].strip())
+                i += 1
+            subtitle['text'] = '\n'.join(text_lines)
+            subtitle['translated'] = ''
+            subtitles.append(subtitle)
+        i += 1
+
+    return subtitles
+
+def dict_to_srt(subtitles, output_file_path):
+    try:
+        with open(output_file_path, 'w', encoding='utf-8') as f:
+            for subtitle in subtitles:
+                f.write(subtitle['id'] + '\n')
+                f.write(subtitle['time-start'] + ' --> ' + subtitle['time-end'] + '\n')
+                f.write(subtitle['translated'] + '\n\n')
+    except Exception as e:
+        print(f"{Fore.RED}ERR{Fore.RESET}: {e}")
+def translate_subtitle(client, subtitle, input_lang, output_lang, pbar, history):
+    max_retries = 4
+    retries = 0
+    new_message = {
+        "role": "user", 
+        "content": f"Translate the following subtitle sentence from {input_lang} to {output_lang}.\
+        Translations must be adapted and grammatically correct according to {output_lang} language.\
+        Use correct genders considering characters names and overall context by referring to this chat history.\
+        Write only the translated sentence. This is the sentence: {subtitle['text']}"}
+    
+    # Append new message to history
+    history.append(new_message)
+    
+    # Keep history at max 30 messages (question + answer)
+    if len(history) > 2*30:
+        history.pop(0)
+    
+    while retries <= max_retries:
+        try:
+            response = client.chat.completions.create(
+                model="gpt-4o-mini",
+                messages=history,
+                web_search=False
+            )
+            history.append({"role":"assistant","content":response.choices[0].message.content})
+            subtitle['translated'] = response.choices[0].message.content
+            pbar.update(1)
+            return
+        except Exception as e:
+            print(f"{Fore.RED}ERR{Fore.RESET}: sub {subtitle['id']} -> {e}, retry {retries + 1}/{max_retries + 1}")
+            print(f"Subtitle request: [{subtitle['text']}]")
+            retries += 1
+            if retries <= max_retries:
+                time.sleep(5)
+            else:
+                print(f"{Fore.RED}ERR{Fore.RESET}: The provider can't translate")
+                subtitle['translated'] = "! TRANSLATION ERROR !"
+                pbar.update(1)
+                return 
+
+def adapt_subtitle(client, subtitle, input_lang, output_lang, pbar, history):
+    max_retries = 4
+    retries = 0
+    new_message = {
+        "role": "user", 
+        "content": f"Considering all translated subtitles in the full chat history, for the following subtitle sentence.\
+        - Adapt the translated text to ensure fluency in {output_lang}.\
+        - If needed fix gender mismatches, using context from the full subtitle history and character names.\
+        - Correct inaccurate translations of short sentences that may have been split across multiple subtitles, considering the broader context.\
+        - Ensure the tone and register match the original context.\
+        - Adapt the translation to sound natural when spoken, avoiding overly literal or bookish phrasing.\
+        If it is already correct, directly rewrite the same sentence; otherwise directly write the corrected sentence.\
+        Original text:{subtitle['text']}\
+        Proposed translation text:{subtitle['translated']}."}
+
+    while retries <= max_retries:
+        try:
+            temp_history = history
+            temp_history.append(new_message)
+            response = client.chat.completions.create(
+                model="gpt-4o-mini",
+                messages=temp_history,
+                web_search=False
+            )
+            subtitle['translated'] = response.choices[0].message.content
+            pbar.update(1)
+            return
+        except Exception as e:
+            print(f"{Fore.RED}ERR{Fore.RESET}: sub {subtitle['id']} -> {e}, retry {retries + 1}/{max_retries + 1}")
+            print(f"Subtitle request: [{subtitle['text']}]")
+            retries += 1
+            if retries <= max_retries:
+                time.sleep(5)
+            else:
+                print(f"{Fore.RED}ERR{Fore.RESET}: The provider can't translate")
+                subtitle['translated'] = "! TRANSLATION ERROR !"
+                pbar.update(1)
+                return 
+
+def check_language(lang_code):
+    try:
+        language = pycountry.languages.get(alpha_2=lang_code) or pycountry.languages.get(alpha_3=lang_code)
+        return language.name.upper()
+    except KeyError:
+        return False
+
+def main():
+    # COLORAMA SETUP
+    init(autoreset=True)
+
+    # ARGUMENTS
+    parser = argparse.ArgumentParser(description="translate SRT file(s) using gpt4free.")
+    parser.add_argument("input_path", help="input path for the .srt file or folder containing .srt files")
+    parser.add_argument("input_lang", help="input language (iso639): eng,fre,ita,jpn..")
+    parser.add_argument("output_lang", help="output language (iso639): eng,fre,ita,jpn..")
+    parser.add_argument("-o", "--output_folder", help="custom output folder for the .srt file(s)")
+    args = parser.parse_args()
+
+    input_path = os.path.abspath(args.input_path)
+    input_lang = check_language(args.input_lang)
+    output_lang = check_language(args.output_lang)
+
+    # ERROR CHECK
+    if not os.path.exists(input_path):
+        print(f"{Fore.RED}ERR{Fore.RESET}: input path not found.")
+        return 1
+    if input_lang == False:
+        print(f"{Fore.RED}ERR{Fore.RESET}: invalid input language (iso639): eng,fre,ita,jpn..")
+        return 1
+    if output_lang == False:
+        print(f"{Fore.RED}ERR{Fore.RESET}: invalid output language (iso639): eng,fre,ita,jpn..")
+        return 1
+
+    # PREPARE OUTPUT FOLDER
+    if args.output_folder is None:
+        output_folder = os.path.dirname(input_path) if os.path.isfile(input_path) else input_path
+    else:
+        output_folder = os.path.abspath(args.output_folder)
+        if not os.path.exists(output_folder):
+            print(f"{Fore.RED}ERR{Fore.RESET}: output folder does not exist.")
+            return 1
+    os.makedirs(output_folder, exist_ok=True)
+
+    # COLLECT SRT FILES
+    srt_files = []
+    if os.path.isfile(input_path) and input_path.endswith('.srt'):
+        srt_files.append(input_path)
+    elif os.path.isdir(input_path):
+        for file in os.listdir(input_path):
+            if file.endswith('.srt'):
+                srt_files.append(os.path.join(input_path, file))
+
+    if not srt_files:
+        print(f"{Fore.RED}ERR{Fore.RESET}: no .srt files found in the specified path.")
+        return 1
+
+    client = Client(provider=Blackbox)
+    history = []
+
+    # PROCESS EACH SRT FILE
+    print(f"{Fore.YELLOW}Loaded {len(srt_files)} SRT file/s{Fore.RESET}")
+    for input_file in srt_files:
+        # SET OUTPUT FILE PATH
+        nome_base, estensione = os.path.splitext(os.path.basename(input_file))
+        output_file = os.path.join(output_folder, f"{nome_base}_{args.output_lang}{estensione}")
+        
+        # SUMMARY
+        in_path,in_file =os.path.split(input_file)
+        out_path,out_file =os.path.split(output_file)
+        print(f"\n{Fore.YELLOW}Input{Fore.RESET}: {Fore.BLUE}{in_path}{Style.BRIGHT}/{in_file}{Style.RESET_ALL}")
+        if os.path.exists(output_file) or input_file.endswith(f"_{args.output_lang}{estensione}"):
+            print(f"{Fore.GREEN}File already translated. Jumping..{Fore.RESET}")
+            continue
+
+        print(f"{Fore.YELLOW}Output{Fore.RESET}: {Fore.BLUE}{out_path}{Style.BRIGHT}/{out_file}{Style.RESET_ALL}")
+        print(f"{Fore.YELLOW}Language{Fore.RESET}: {Fore.GREEN}{input_lang}{Fore.RESET}->{Fore.GREEN}{output_lang}{Fore.RESET}")
+
+        # PARAMETERS
+        subtitles_list = srt_to_dict(input_file)
+
+        # TRANSLATION
+        with tqdm(total=len(subtitles_list), desc=f"{Fore.YELLOW}Initial Translation (1/2){Fore.RESET}") as pbar:
+            for subtitle in subtitles_list:
+                translate_subtitle(client, subtitle, input_lang, output_lang, pbar, history)
+        
+        with tqdm(total=len(subtitles_list), desc=f"{Fore.YELLOW}Final Adaptation (2/2){Fore.RESET}") as pbar:
+            for subtitle in subtitles_list:
+                # craft the updated history by stringifing subtitles
+                full_translated_text = ""
+                for subtitle_t in subtitles_list:
+                    full_translated_text += f"Text [{subtitle_t["id"]}]:{subtitle_t['text']}\nTranslated [{subtitle_t["id"]}]:{subtitle_t['translated']}\n\n"
+                history = [{"role":"assistant","content":full_translated_text}]
+                # adapt
+                adapt_subtitle(client, subtitle, input_lang, output_lang, pbar, history)
+        
+        # FINAL SAVE
+        dict_to_srt(subtitles_list, output_file)
+
+if __name__ == "__main__":
+    main()
